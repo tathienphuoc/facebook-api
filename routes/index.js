22de74e1c252c76301cb1178caa8f5acdbf2edfb
tathienphuoc <tathienphuoc@gmail.com>
2022-05-10 01:00:02 +0700

    Add vercel.json

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-router.use("/", (req, res) => {
+router.get("/", (req, res) => {
   res.json("hello");
 });
 router.use("/users", require("./user"));
