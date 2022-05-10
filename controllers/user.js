0a42fef2315ce1fbf778b0fcbb881adc22eab476
tathienphuoc <tathienphuoc@gmail.com>
2022-05-10 17:06:02 +0700

    Cros problem

--- controllers/user.js
+++ controllers/user.js
@@ -76,7 +76,7 @@ module.exports = {
     res.cookie("access_token", accessToken, {
       maxAge: 365 * 24 * 60 * 60 * 100, // thời gian sống
       httpOnly: true, // chỉ có http mới đọc được token
-      //secure: true; //ssl nếu có, nếu chạy localhost thì comment nó lại
+      secure: true, //ssl nếu có, nếu chạy localhost thì comment nó lại
     });
 
     return res.status(200).json({ ...User.toDTO(user) });
